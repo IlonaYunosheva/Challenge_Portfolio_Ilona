0c0cf8b120d099db24cf32fc9cee68410c0cb1b3
IlonaYunosheva <yunoshevailona@gmail.com>
2023-01-24 12:14:53 +0100

    Second task

---
+++ pages/addingamatchform.py
@@ -0,0 +1,15 @@
+from pages.base_page import BasePage
+
+
+class addingamatchform(BasePage):
+   my_team_field_xpath "//input[@name="myTeam"]"
+   enemy_tam_field_xpath "//input[@name="enemyTeam"]"
+   my_team_score_field_xpath "//input[@name="myTeamScore"]"
+   enemy_team_score_field_xpath "//input[@name="enemyTeamScore"]"
+   date_field_xpath "//*[@id="__next"]/div[1]/main/div[2]/form/div[2]/div/div[5]/div/div/input"
+   match_at_home_button_xpath "//*[@name="matchAtHome"]"
+   match_out_home_button_xpath "//*[@id="__next"]/div[1]/main/div[2]/form/div[2]/div/div[6]/fieldset/div/label[2]/span[1]/span[1]/input"
+   web_match_button_xpath "//*[@name="webMatch"]"
+   general_button_xpath "//*[@name="general"]"
+   time_played_field_xpath "//*[@name="timePlayed"]"
+
---
+++ pages/dashboard.py
@@ -0,0 +1,15 @@
+from pages.base_page import BasePage
+
+
+class Dashboard(BasePage):
+    button_xpath = "//*[id='login']"
+    main_page_button_xpath = "//*[text()="Main page"]"
+    players_button_xpath = "//*[text()="Players"]"
+    language_button_xpath = "//*[@id="__next"]/div[1]/div/div/div/ul[2]/div[1]/div[2]/span"
+    sing_out_button_xpath = "//*[@id="__next"]/div[1]/div/div/div/ul[2]/div[2]/div[2]/span"
+    search_field_xpath = "//*[@class="MuiInputBase-input jss51"]"
+    download_CSV_button_xpath = "//*[@id="__next"]/div[1]/main/div[2]/div/div/div[1]/div[2]/button"
+    print_button_xpath = "//*[@id="__next"]/div[1]/main/div[2]/div/div/div[1]/div[2]/span[1]/span/button"
+    view_columns_button_xpath = "//*[@id="__next"]/div[1]/main/div[2]/div/div/div[1]/div[2]/span[2]/button"
+    filter_table_xpath = "//*[@id="__next"]/div[1]/main/div[2]/div/div/div[1]/div[2]/span[3]/button"
+    next_page_button_xpath = "//*[@id="pagination-next"]"
